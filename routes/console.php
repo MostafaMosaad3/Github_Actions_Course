747138219aefcbeab145a2dcba2fe9123cbd7b93
mostafa.mossad <mostafamosaad202@gmail.com>
2026-06-08 13:18:48 +0300

    example 02 - formating with laravel pint

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,3 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-// add new line
-// add new line\\\\
-\\\
